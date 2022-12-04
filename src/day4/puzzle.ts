3fc52906eb2985471a2da7bdc59ecedd3a2912ba
Alexander Herback <alexander.herback@wearelevels.com>
2022-12-04 19:26:55 +0100

    day4

--- src/day4/puzzle.ts
+++ src/day4/puzzle.ts
@@ -1,11 +1,52 @@
 import { data } from './input';
 
+const populateRange = (range: string) => {
+  const ranges = range.split('-');
+  let startRange: number = +ranges[0];
+  const endRange: number = +ranges[1];
+  const length = endRange - startRange;
+
+  return Array(length + 1)
+    .fill(null)
+    .map(() => {
+      return startRange++;
+    });
+};
+
 const problem1 = () => {
-  return 'hello world 1';
+  return data.reduce((accumulator, line) => {
+    const pair = line.split(',');
+    const firstRange = populateRange(pair[0]);
+    const secondRange = populateRange(pair[1]);
+
+    const biggestList =
+      firstRange.length > secondRange.length ? firstRange : secondRange;
+    const smallestList = firstRange === biggestList ? secondRange : firstRange;
+
+    return smallestList.filter((element) => {
+      return !biggestList.includes(element);
+    }).length > 0
+      ? accumulator
+      : accumulator + 1;
+  }, 0);
 };
 
 const problem2 = () => {
-  return data;
+  return data.reduce((accumulator, line) => {
+    const pair = line.split(',');
+    const firstRange = populateRange(pair[0]);
+    const secondRange = populateRange(pair[1]);
+
+    const biggestList =
+      firstRange.length > secondRange.length ? firstRange : secondRange;
+    const smallestList = firstRange === biggestList ? secondRange : firstRange;
+
+    return smallestList.filter((element) => {
+      return biggestList.includes(element);
+    }).length > 0
+      ? accumulator + 1
+      : accumulator;
+  }, 0);
 };
 
 const main = () => {
