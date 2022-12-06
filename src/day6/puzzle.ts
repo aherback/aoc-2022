f24c25fa3d2b5255bd91f85d4a7aeda858636d74
Alexander Herback <alexander.herback@wearelevels.com>
2022-12-06 23:04:14 +0100

    day6

--- src/day6/puzzle.ts
+++ src/day6/puzzle.ts
@@ -1,16 +1,23 @@
 import { data } from './input';
 
+const getMark = (messageLength: number) => {
+  return (
+    [...Array(data.length).keys()].find((key) => {
+      const compareSet = [...new Set(data.substring(key, key + messageLength))];
+
+      if (compareSet.length >= messageLength) {
+        return key;
+      }
+    }) + messageLength
+  );
+};
 const problem1 = () => {
-  return 'hello world 1';
+  return getMark(4);
 };
 
 const problem2 = () => {
-  return data;
+  return getMark(14);
 };
 
-const main = () => {
-  console.log(problem1());
-  console.log(problem2());
-};
-
-main();
+console.log(problem1());
+console.log(problem2());
